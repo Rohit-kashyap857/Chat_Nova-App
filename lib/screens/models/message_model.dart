e9cced430c1cd1051b10fbd6a1974ddc5035491b
Rohit <kashyaprohit9214@gmail.com>
2026-02-27 10:27:35 +0530

    Add MessageModel class for message data handling

---
+++ lib/screens/models/message_model.dart
@@ -0,0 +1,25 @@
+import 'package:hive/hive.dart';
+
+part 'message_model.g.dart';
+
+@HiveType(typeId: 0)
+class MessageModel {
+  @HiveField(0)
+  final String id;
+
+  @HiveField(1)
+  final String text;
+
+  @HiveField(2)
+  final bool isUser;
+
+  @HiveField(3)
+  final DateTime timestamp;
+
+  MessageModel({
+    required this.id,
+    required this.text,
+    required this.isUser,
+    required this.timestamp,
+  });
+}
